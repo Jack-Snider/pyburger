1251f166aa2f463c2b0e585e2df77b94155a1b07
= <choiman2021@gmail.com>
2023-04-03 21:57:51 +0900

    admin계정 만들고 데이터 집어넣기

--- burgers/admin.py
+++ burgers/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from burgers.models import Burger
 
 # Register your models here.
+
+@admin.register( Burger )
+class BurgerAdmin( admin.ModelAdmin ): # burgers앱에 관한 관리자
+    pass
--- burgers/models.py
+++ burgers/models.py
@@ -13,3 +13,6 @@ class Burger( models.Model ): # 햄버거를 나타내는 Model 클래스 정의
     name = models.CharField( max_length = 20 ) # 문자열을 저장하는 CharField
     price = models.IntegerField( default = 0 ) # 숫자를 저장하는 IntegerField
     calories = models.IntegerField( default = 0 )  # 숫자를 저장하는 IntegerField
+
+    def __str__( self ): # 해당 메서드는 모델 클래스의 인스턴스를 어떻게 표현할지 나타낸다.
+        return self.name
